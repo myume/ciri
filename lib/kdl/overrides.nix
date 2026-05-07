82f74df687d1729cd846adb99b7ca065b89e2f27
yum <contact@meyume.com>
2026-05-07 17:30:34 -0400

    feat: handle matches

--- lib/kdl/overrides.nix
+++ lib/kdl/overrides.nix
@@ -2,28 +2,44 @@
   lib,
   utils,
 }: let
-  inherit (utils) sectionsToString indentSection;
+  inherit (utils) sectionsToString indentSection mapNull;
   toBoolArg = name: value: "${name} ${lib.boolToString value}";
   toKDLString = value:
     if builtins.isBool value
     then lib.boolToString value
+    else if builtins.isString value
+    then "\"${value}\""
     else toString value;
+
+  flattenAttrEntries =
+    lib.mapAttrsToList (ele_name: ele_val: mapNull (val: "${ele_name} ${toKDLString val}") ele_val);
+
+  matchesToKDL = _: value: let
+    matches = lib.pipe value [
+      (map flattenAttrEntries)
+      lib.flatten
+      utils.filterNulls
+      (map (builtins.replaceStrings [" "] ["="]))
+      (map (s: "match ${s}"))
+    ];
+  in
+    sectionsToString matches;
 in {
   "window-rules.window-rule.background-effect.blur" = toBoolArg;
   "window-rules.window-rule.background-effect.xray" = toBoolArg;
   "window-rules.window-rule.clip-to-geometry" = toBoolArg;
+  "window-rules.window-rule.matches" = matchesToKDL;
 
   "layer-rules.layer-rule.place-within-backdrop" = toBoolArg;
   "layer-rules.layer-rule.background-effect.blur" = toBoolArg;
   "layer-rules.layer-rule.background-effect.xray" = toBoolArg;
+  "layer-rules.layer-rule.matches" = matchesToKDL;
 
   "layout.shadow.offset" = name: value: "${name} x=${toString value.x} y=${toString value.y}";
 
   "layout.preset-column-widths" = name: value: let
     sections = lib.flatten (
-      map (
-        lib.mapAttrsToList (ele_name: ele_val: "${ele_name} ${toKDLString ele_val}")
-      )
+      map flattenAttrEntries
       value
     );
   in ''
